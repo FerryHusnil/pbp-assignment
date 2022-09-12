016308ad9a4a22526e37d469187c321fb9e34210
Mohammad Ferry Husnil Arif <ferryhusnil07@gmail.com>
2022-09-12 17:52:45 +0700

    finish build web and ready to deploy

--- katalog/apps.py
+++ katalog/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class KatalogConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'katalog'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "katalog"
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_catalog_item = CatalogItem.objects.all()
+    context = {
+        "data_catalog_item": data_catalog_item,
+        "name": "Mohammad Ferry Husnil Arif",
+        "student_id": "2106709112",
+    }
+    return render(request, "katalog.html", context)
