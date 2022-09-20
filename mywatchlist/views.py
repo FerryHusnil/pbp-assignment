bda23e05afb9def20d814584f925214aa495d21c
Mohammad Ferry Husnil Arif <ferryhusnil07@gmail.com>
2022-09-20 16:19:57 +0700

    add message for bonus assignment

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -6,10 +6,17 @@ from django.core import serializers
 # Create your views here.
 def show_html(request):
     data_mywatchlist_item = MyWatchList.objects.all()
+    watched_count = len(MyWatchList.objects.filter(watched=True))
+    not_watched_count = len(MyWatchList.objects.filter(watched=False))
+    if watched_count >= not_watched_count:
+        msg = "Selamat, kamu sudah banyak menonton!"
+    else:
+        msg = "Wah, kamu masih sedikit menonton!"
     context = {
         "data_mywatchlist_item": data_mywatchlist_item,
         "name": "Mohammad Ferry Husnil Arif",
         "student_id": "2106709112",
+        "msg": msg,
     }
     return render(request, "mywatchlist.html", context)
 
