b68ef780375f08afabb5e045149ee6fdc51bc7ca
Mohammad Ferry Husnil Arif <ferryhusnil07@gmail.com>
2022-09-22 09:57:00 +0700

    update endpoint on tests.py

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -3,14 +3,14 @@ from django.test import TestCase
 # Create your tests here.
 class MyWatchlistTests(TestCase):
     def test_html_endpoint(self):
-        resp = self.client.get("/mywatchlist/html/")
+        resp = self.client.get("/mywatchlist/html")
         self.assertEqual(resp.status_code, 200)
 
     def test_xml_endpoint(self):
-        resp = self.client.get("/mywatchlist/xml/")
+        resp = self.client.get("/mywatchlist/xml")
         print(resp)
         self.assertEqual(resp.status_code, 200)
 
     def test_json_endpoint(self):
-        resp = self.client.get("/mywatchlist/json/")
+        resp = self.client.get("/mywatchlist/json")
         self.assertEqual(resp.status_code, 200)
